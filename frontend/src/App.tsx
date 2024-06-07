001779ae96991944e454ab16a79bb610be01b78b
MasterofGames77 <mgambardella16@gmail.com>
2024-06-07 12:11:22 -0400

    updated login function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,46 @@
+// App.tsx
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Login from "./components/Login";
+import Home from "./components/Home";
 import Register from "./components/Register";
 import UserPosts from "./components/UserPosts";
 import CreatePost from "./components/CreatePost";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App: React.FC = () => {
+  const isAuthenticated = !!localStorage.getItem("token");
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/user/:id" element={<UserPosts />} />
-        <Route path="/create" element={<CreatePost />} />
+        <Route
+          path="/user/:id"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <UserPosts />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/create"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <CreatePost />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/"
+          element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
+        />
       </Routes>
     </Router>
   );
